a8389860486cba2ddb8bd61c81aa3107c88cc743
aelkheta <khatabiaziz707@gmail.com>
2024-03-10 20:12:13 +0000

    push_swap

--- push_swap_tester.sh
+++ push_swap_tester.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Set text color
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 
@@ -12,7 +11,6 @@ RED_BOLD='\033[1;31m'
 BG_GREEN='\033[48;5;22m'
 BG_RED='\033[48;5;124m'
 
-# Reset text color
 NC='\033[0m'
 
 
@@ -32,17 +30,27 @@ successful_tests=0
 
 for ((i = 0; i < test_count; i++)); do
     args=$(seq $min_range $max_range | shuf -n $arg_count | tr '\n' ' ')
-    output=$(./push_swap $args | ./checker_linux $args)
 
-    if [[ $output == *"OK"* ]]; then
-        echo -e "${GREEN_BOLD}\n=============> Test $i OK <=============${NC}"
+    output=$(./push_swap $args | ./checker_linux $args)
+    moves=$(./push_swap $args | wc -l)
+    if [[ $output == *"OK"* && $arg_count -eq 100 && $moves -lt 700 ]]; then
+        echo -e "\n${GREEN_BOLD}========> Test $i OK <========${NC}"
+        echo -e "\n${BG_GREEN}${WHITE_BOLD} $moves moves ${NC}"
+        successful_tests=$((successful_tests+1))
+    elif [[ $output == *"OK"* && $arg_count -eq 500 && $moves -lt 5500 ]]; then
+        echo -e "\n${GREEN_BOLD}========> Test $i OK <========${NC}"
+        echo -e "\n${BG_GREEN}${WHITE_BOLD} $moves moves ${NC}"
+        successful_tests=$((successful_tests+1))
+    elif [[ $output == *"OK"* && $arg_count -gt 500 ]]; then
+        echo -e "\n${GREEN_BOLD}========> Test $i OK <========${NC}"
+        echo -e "\n${BG_GREEN}${WHITE_BOLD} $moves moves ${NC}"
         successful_tests=$((successful_tests+1))
     else
-        echo -e "${RED_BOLD}\n========> Test $i KO <========${NC}"
+        echo -e "\n${RED_BOLD}========> Test $i KO <========${NC}"
+        echo -e "\n${BG_RED}${WHITE_BOLD}$moves moves ${NC}"
     fi
 done
 
-# Check if all tests were successful
 if [[ $successful_tests -eq $test_count ]]; then
     echo -e "${GREEN}\n\t\t ██████╗ ██████╗ ███╗   ██╗ ██████╗ ██████╗  █████╗ ████████╗███████╗${NC}"
     echo -e "${GREEN}\t\t██╔════╝██╔═══██╗████╗  ██║██╔════╝ ██╔══██╗██╔══██╗╚══██╔══╝██╔════╝${NC}"
